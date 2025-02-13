c2ad9bc1e2589844430d508fd081f22bd30a4177
jorgedmgb <74454645+jorgedmgb@users.noreply.github.com>
2025-02-13 19:04:31 +0100

    ImagenesYStatsMetidas

--- JS/Script.js
+++ JS/Script.js
@@ -1,24 +1,101 @@
 // Scripts para la web 
 
 
-// pagina selector personajes
-function ChangeCharacter(character) {
+let player1Selected = false;
+let player2Selected = false;
+
+function selectCharacter(character) {
+    if (!player1Selected) {
+        updatePlayerStats('player1', character);
+        player1Selected = true;
+    } else if (!player2Selected) {
+        updatePlayerStats('player2', character);
+        player2Selected = true;
+    }
+
+    if (player1Selected && player2Selected) {
+        document.getElementById('ready-button').disabled = false;
+    }
+}
+
+function updatePlayerStats(player, character) {
+    const playerImg = document.getElementById(`${player}-img`);
+    const playerInfo = document.getElementById(`${player}-info`);
+    let characterName, characterImage, characterStats;
+
     switch(character) {
-        case "bryan":
-            document.getElementById("img").src = "images/bryan.png";
-            document.getElementById("name").innerHTML = "Bryan Fury";
+        case 'james':
+            characterName = "James";
+            characterImage = "images/james.png";
+            characterStats = { Fuerza: 80, Velocidad: 70, Vida: 60 };
             break;
-        case "jin":
-            document.getElementById("img").src = "images/jin.png";
-            document.getElementById("name").innerHTML = "Jin Kazama";
+        case 'emily':
+            characterName = "Emily";
+            characterImage = "images/emily.png";
+            characterStats = { Fuerza: 60, Velocidad: 80, Vida: 70 };
             break;
-        case "paul":
-            document.getElementById("img").src = "images/paul.png";
-            document.getElementById("name").innerHTML = "Paul Phoenix";
+        case 'william':
+            characterName = "William";
+            characterImage = "images/william.png";
+            characterStats = { Fuerza: 90, Velocidad: 60, Vida: 50 };
             break;
-        case "hwoarang":
-            document.getElementById("img").src = "images/hwoarang.png";
-            document.getElementById("name").innerHTML = "Hwoarang";
+        case 'olivia':
+            characterName = "Olivia";
+            characterImage = "images/olivia.png";
+            characterStats = { Fuerza: 50, Velocidad: 90, Vida: 80 };
+            break;
+        case 'michael':
+            characterName = "Michael";
+            characterImage = "images/michael.png";
+            characterStats = { Fuerza: 70, Velocidad: 70, Vida: 70 };
+            break;
+        case 'charlotte':
+            characterName = "Charlotte";
+            characterImage = "images/charlotte.png";
+            characterStats = { Fuerza: 60, Velocidad: 60, Vida: 90 };
+            break;
+        case 'benjamin':
+            characterName = "Benjamin";
+            characterImage = "images/benjamin.png";
+            characterStats = { Fuerza: 85, Velocidad: 65, Vida: 55 };
+            break;
+        case 'sophia':
+            characterName = "Sophia";
+            characterImage = "images/sophia.png";
+            characterStats = { Fuerza: 55, Velocidad: 85, Vida: 75 };
+            break;
+        case 'henry':
+            characterName = "Henry";
+            characterImage = "images/henry.png";
+            characterStats = { Fuerza: 75, Velocidad: 75, Vida: 65 };
+            break;
+        case 'isabella':
+            characterName = "Isabella";
+            characterImage = "images/isabella.png";
+            characterStats = { Fuerza: 65, Velocidad: 75, Vida: 85 };
+            break;
+        case 'alexander':
+            characterName = "Alexander";
+            characterImage = "images/alexander.png";
+            characterStats = { Fuerza: 80, Velocidad: 60, Vida: 70 };
+            break;
+        case 'grace':
+            characterName = "Grace";
+            characterImage = "images/grace.png";
+            characterStats = { Fuerza: 50, Velocidad: 80, Vida: 90 };
             break;
     }
+
+    playerImg.src = characterImage;
+    playerImg.alt = characterName;
+    playerInfo.innerHTML = `
+        <h3>${characterName}</h3>
+        <p>Fuerza: <div class="stat-bar"><div class="stat-fill fuerza" style="width: ${characterStats.Fuerza}%;"></div></div></p>
+        <p>Velocidad: <div class="stat-bar"><div class="stat-fill velocidad" style="width: ${characterStats.Velocidad}%;"></div></div></p>
+        <p>Vida: <div class="stat-bar"><div class="stat-fill vida" style="width: ${characterStats.Vida}%;"></div></div></p>
+    `;
+}
+
+function ready() {
+    alert("¡READY!");
 }
