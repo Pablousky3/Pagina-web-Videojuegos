226ee837ceb3b6e1a01ad2cd1f504730f2ffe211
DESKTOP-65SAUST\Alumno <jorgedmgb@gmail.com>
2025-02-13 15:36:03 +0100

    CharacterSelector

--- JS/Script.js
+++ JS/Script.js
@@ -1,1 +1,24 @@
 // Scripts para la web 
+
+
+// pagina selector personajes
+function ChangeCharacter(character) {
+    switch(character) {
+        case "bryan":
+            document.getElementById("img").src = "images/bryan.png";
+            document.getElementById("name").innerHTML = "Bryan Fury";
+            break;
+        case "jin":
+            document.getElementById("img").src = "images/jin.png";
+            document.getElementById("name").innerHTML = "Jin Kazama";
+            break;
+        case "paul":
+            document.getElementById("img").src = "images/paul.png";
+            document.getElementById("name").innerHTML = "Paul Phoenix";
+            break;
+        case "hwoarang":
+            document.getElementById("img").src = "images/hwoarang.png";
+            document.getElementById("name").innerHTML = "Hwoarang";
+            break;
+    }
+}
