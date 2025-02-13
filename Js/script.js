3fb26c1e7f8daf1e191698faacfd7551d0d18417
DESKTOP-65SAUST\Alumno <informatica@esne.es>
2025-02-13 15:49:26 +0100

    Hover icono (la animación no para aunque quites el ratón de encima)

--- Js/script.js
+++ Js/script.js
@@ -1,2 +1,9 @@
 
 // console.log("elpepe");
+
+let hovericon = $("i.fa-solid.fa-location-pin.fa-beat-fade");
+
+$("i.fa-solid.fa-location-pin.fa-beat-fade").mouseover(function(){
+    $("i.fa-solid.fa-location-pin.fa-beat-fade").css("animation-iteration-count", "infinite")
+})
+
