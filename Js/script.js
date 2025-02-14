9e8f4d177bbfd11f21e96a90e1ab9b581bb7ee7e
FreSst1 <ricardo.arevaloferreira@gmail.com>
2025-02-14 14:07:35 +0100

    Funcionamiento hover de icono imagen

--- Js/script.js
+++ Js/script.js
@@ -3,7 +3,12 @@
 
 let hovericon = $("i.fa-solid.fa-location-pin.fa-beat-fade");
 
-$("i.fa-solid.fa-location-pin.fa-beat-fade").mouseover(function(){
+$("i.fa-solid.fa-location-pin.fa-beat-fade").mouseenter(function(){
     $("i.fa-solid.fa-location-pin.fa-beat-fade").css("animation-iteration-count", "infinite")
+    $(".FotoCanada img").css("opacity", "100%")
 })
 
+$("i.fa-solid.fa-location-pin.fa-beat-fade").mouseleave(function(){
+    $("i.fa-solid.fa-location-pin.fa-beat-fade").css("animation-iteration-count", "0")
+    $(".FotoCanada img").css("opacity", "0")
+})
