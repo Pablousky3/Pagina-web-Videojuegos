0f5f9fc4dce6bb0e726ddc8ee6b730e068525689
FreSst1 <ricardo.arevaloferreira@gmail.com>
2025-02-14 17:31:21 +0100

    Primer punto completo

--- Js/script.js
+++ Js/script.js
@@ -6,9 +6,11 @@ let hovericon = $("i.fa-solid.fa-location-pin.fa-beat-fade");
 $("i.fa-solid.fa-location-pin.fa-beat-fade").mouseenter(function(){
     $("i.fa-solid.fa-location-pin.fa-beat-fade").css("animation-iteration-count", "infinite")
     $(".FotoCanada img").css("opacity", "100%")
+    $(".TextoCanada").css("opacity", "100%")
 })
 
 $("i.fa-solid.fa-location-pin.fa-beat-fade").mouseleave(function(){
     $("i.fa-solid.fa-location-pin.fa-beat-fade").css("animation-iteration-count", "0")
     $(".FotoCanada img").css("opacity", "0")
+    $(".TextoCanada").css("opacity", "0")
 })
